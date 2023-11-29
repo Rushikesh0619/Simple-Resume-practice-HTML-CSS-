e809925b0b8653cff93c03832c1c49e2e9ca1268
Rushikesh0619 <102864049+Rushikesh0619@users.noreply.github.com>
2023-11-29 22:00:18 +0530

    Create hook2.jsx

---
+++ hook2.jsx
@@ -0,0 +1,69 @@
+import React, { useRef, useState } from 'react';
+
+const Form = () => {
+  // useRef for each form input and their corresponding error messages
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const [nameError, setNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  // Validation functions
+  const validateName = () => {
+    const name = nameRef.current.value.trim();
+    setNameError(name ? '' : 'Name is required');
+  };
+
+  const validateEmail = () => {
+    const email = emailRef.current.value.trim();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    setEmailError(emailRegex.test(email) ? '' : 'Invalid email address');
+  };
+
+  const validatePassword = () => {
+    const password = passwordRef.current.value.trim();
+    setPasswordError(password.length >= 6 ? '' : 'Password must be at least 6 characters');
+  };
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Check if all inputs are valid before submitting
+    if (nameError || emailError || passwordError) {
+      alert('Please fix the form errors before submitting.');
+    } else {
+      // Submit the form or perform any other action
+      alert('Form submitted successfully!');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Name:</label>
+        <input type="text" ref={nameRef} onBlur={validateName} />
+        <span style={{ color: 'red' }}>{nameError}</span>
+      </div>
+
+      <div>
+        <label>Email:</label>
+        <input type="text" ref={emailRef} onBlur={validateEmail} />
+        <span style={{ color: 'red' }}>{emailError}</span>
+      </div>
+
+      <div>
+        <label>Password:</label>
+        <input type="password" ref={passwordRef} onBlur={validatePassword} />
+        <span style={{ color: 'red' }}>{passwordError}</span>
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
