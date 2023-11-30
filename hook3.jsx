3418207eaa0f76d3d16d1d5a3bab1692b96db669
Rushikesh0619 <102864049+Rushikesh0619@users.noreply.github.com>
2023-11-30 09:06:13 +0530

    Update hook3.jsx

--- hook3.jsx
+++ hook3.jsx
@@ -3,16 +3,6 @@ import React, { useState, useEffect, useCallback } from 'react';
 const List = () => {
   const [items, setItems] = useState([]);
 
-  const ListItem = useCallback(({ item }) => {
-    console.log(`Rendering item: ${item.id}`);
-    return (
-      <div>
-        <strong>{item.title}</strong>
-        <p>{item.body}</p>
-      </div>
-    );
-  }, []);
-
   useEffect(() => {
     // Fetch data from a public API
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -20,12 +10,19 @@ const List = () => {
       .then((data) => setItems(data.slice(0, 5))); // Displaying only the first 5 items
   }, []);
 
+  const renderItems = useCallback(() => {
+    return items.map((item) => (
+      <div key={item.id}>
+        <strong>{item.title}</strong>
+        <p>{item.body}</p>
+      </div>
+    ));
+  }, [items]);
+
   return (
     <div>
       <h1>List Component</h1>
-      {items.map((item) => (
-        <ListItem key={item.id} item={item} />
-      ))}
+      {renderItems()}
     </div>
   );
 };
